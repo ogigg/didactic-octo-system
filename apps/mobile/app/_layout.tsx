1138e756db4e6940643ebbac6a1baf7c6e032131
Oskar Gierszewski <gierszon10@gmail.com>
2025-10-13 00:09:33 +0200

    Add expo app

---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { greeting } from 'mobile-package';
+import { Text } from 'react-native';
+import 'react-native-reanimated';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Text>{greeting}</Text>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
