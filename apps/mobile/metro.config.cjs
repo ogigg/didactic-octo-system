bbe0df267736edc94c84678fd5907cd64fd9b3b2
Oskar Gierszewski <gierszon10@gmail.com>
2026-05-14 23:53:25 +0200

    Update package dependencies and configuration

--- apps/mobile/metro.config.cjs
+++ apps/mobile/metro.config.cjs
@@ -7,6 +7,7 @@ const monorepoRoot = path.resolve(projectRoot, "../..");
 const config = getDefaultConfig(projectRoot);
 
 config.watchFolders = [monorepoRoot];
+config.resolver.disableHierarchicalLookup = true;
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(monorepoRoot, "node_modules"),
